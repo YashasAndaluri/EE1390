ba86bff6d5426e87fba48dc8619b4646f790bb9c
YashasAndaluri <es17btech11025@iith.ac.in>
2019-02-14 01:02:41 +0530

    Added latex beamer files

--- codes/Q16_MAIN14_G.py
+++ codes/Q16_MAIN14_G.py
@@ -18,6 +18,7 @@ dvec=np.array([-1,1])
 d=np.matmul(PS,dvec)
 ##Unit direction vector:
 unit_d=d/(np.linalg.norm(d))
+print(unit_d)
 ##Let A be the point (1,-1)
 A=np.array([1,-1])
 #Let AB be the line parallel to PS where B is a point on the line
